2ef1eaf6b39c5fe3f6695f16e0c066cc6b2db457
Anthony Gyselman <anthony.gyselman@gmail.com>
2022-03-24 10:30:35 +0100

    first exo

---
+++ exo.php
@@ -0,0 +1,11 @@
+<?php
+    $maVariable = 5;
+    echo $maVariable;
+    echo gettype ($maVariable);
+    echo "<br>";
+    $monString = 'Anthony';
+    echo $monString;
+
+    $monBoolean = false;
+    echo gettype ($monBoolean);
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- <?php
+    // bonsoir je suis un commentaire
+    /*
+    je suis un commentaire sur plusieurs ligne
+    */
+    phpinfo();
+
+
+?> -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+        echo " Bonjour";
+    ?>
+
+
+    <?php
+    $maVariable = 5;
+    ?>
+</body>
+</html>
