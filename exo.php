91c6a646cb349ad293f6f5343ce5b16406e33142
Anthony Gyselman <anthony.gyselman@gmail.com>
2022-03-24 11:11:05 +0100

    exo addition

--- exo.php
+++ exo.php
@@ -1,11 +1,27 @@
-<?php
+<!-- <?php
     $maVariable = 5;
     echo $maVariable;
     echo gettype ($maVariable);
     echo "<br>";
     $monString = 'Anthony';
     echo $monString;
-
+    echo "<br>";
     $monBoolean = false;
     echo gettype ($monBoolean);
+?> -->
+
+<!-- Exemple Concaténation -->
+<!-- <?php
+    $prenom = 'Anthony';
+    echo 'mon prénom : '.$prenom.'<br>';
+    echo "mon prénom : $prenom<br>";
+    echo "mon prénom : {$prenom}<br>";  
+?> -->
+
+<?php
+    $a = 12;
+    $b = 10;
+
+    $total = $a + $b;
+    echo $total;
 ?>
