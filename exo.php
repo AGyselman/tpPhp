70500d5d54d69c9a953007b0e7f35bba598017f8
Anthony Gyselman <anthony.gyselman@gmail.com>
2022-03-24 14:40:20 +0100

    exo function1

--- exo.php
+++ exo.php
@@ -31,29 +31,68 @@
 <?php
 
 // EXO 2
-    $a = 5;
-    $b = 3;
-    $c = $a + $b;
-    echo "$a<br>"; 
-    echo "$b<br>"; 
-    echo "$c<br>"; 
+    // $a = 5;
+    // $b = 3;
+    // $c = $a + $b;
+    // echo "$a<br>"; 
+    // echo "$b<br>"; 
+    // echo "$c<br>"; 
 
-    $a = 2;
-    echo "$a<br>";
-    $c = $b - $a;
-    echo "$a<br>";
-    echo "$b<br>"; 
-    echo "$c<br>"; 
+    // $a = 2;
+    // echo "$a<br>";
+    // $c = $b - $a;
+    // echo "$a<br>";
+    // echo "$b<br>"; 
+    // echo "$c<br>"; 
 
 // EXO 3
 
-    $a = 15;
-    $b = 23;
-    $c = $a;
-    $a = $b;
-    $b = $c;
-    echo "$a<br>";
-    echo "$b<br>";
+    // $a = 15;
+    // $b = 23;
+    // $c = $a;
+    // $a = $b;
+    // $b = $c;
+    // echo "$a<br>";
+    // echo "$b<br>";
 
+// EXEMPLE FONCTION
+
+// function addition($a,$b)
+//     {
+//         /* $total = $a+$b;
+//         return $total; */
+//         return $a+$b;
+//     }
+//     echo addition(17,23);
+//     $a = 10;
+//     $b = 27;
+//     echo "<script> 
+//         console.log($a);
+//         console.log($b);
+//         </script>";
+
+
+// EXO 4 
+// $prixHt =15;
+// $nbArticle = 22;
+// $tauxTva = 0.2;
+// function article($a,$b,$c)
+// {
+//     echo "$a <br>";
+//     echo "$b <br>";
+//     echo "$c <br>";
+//     return $a*$b*$c;
+// }
+// echo article($prixHt,$nbArticle,$tauxTva);
+
+function nombre($a) {
+if($a >= 0)
+{
+    echo "le chiffre est positif";
+} else {
+    echo "le chiffre est négatif";
+}
+}
+echo nombre(-2);
 ?>
 
